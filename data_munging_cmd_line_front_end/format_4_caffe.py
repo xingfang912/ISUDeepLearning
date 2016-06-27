41b404834507f12e9e62602479753c271a86d841
Ryan Dellana <ryan.dellana@gmail.com>
2016-06-27 15:47:35 -0400

    Update and rename format_4_caffe.py to data_munging_cmd_line_front_end/format_4_caffe.py

--- data_munging_cmd_line_front_end/format_4_caffe.py
+++ data_munging_cmd_line_front_end/format_4_caffe.py
@@ -4,5 +4,6 @@ import argparse
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Format images in class subfolders for caffe. Generate lmdb file.')
     parser.add_argument('--width', dest='width', type=int, required=True, help='width of output images.')
+    parser.add_argument('--path', dest='path', type=str, required=True, help='root path where image class folders are stored.')
     args = parser.parse_args()
-    DataMungingUtil.format_4_caffe(target_width=args.width)
+    DataMungingUtil.format_4_caffe(path=args.path, target_width=args.width)
