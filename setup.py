956443fea9935406ea7e97bf6c625f389996b3a2
Ryan Dellana <ryan.dellana@gmail.com>
2016-08-05 02:39:07 -0400

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='ISUDeepLearning',
+      version='0.1',
+      description='Deep learning research tools',
+      url='https://github.com/xingfang912/ISUDeepLearning',
+      author='Xing Fang and Ryan Dellana',
+      author_email='xingfang912@gmail.com; ryan.dellana@gmail.com',
+      license='GNU',
+      packages=['ISUDeepLearning'],
+      zip_safe=False)
